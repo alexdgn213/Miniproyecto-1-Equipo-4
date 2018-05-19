61efd1a510ec649df58c82ec74b3c33385ccf111
marcolbx <marcolbx@hotmail.com>
2018-05-18 22:02:16 -0400

    Agregados mas Articulos
    Agregados:
    Nestea
    Pizza
    Huevos
    Cocacola

--- app/src/main/java/android/example/com/miniproyecto1_equipo4/MainActivity.java
+++ app/src/main/java/android/example/com/miniproyecto1_equipo4/MainActivity.java
@@ -35,9 +35,16 @@ public class MainActivity extends AppCompatActivity {
         private void inicializarData(){
             articulos = new ArrayList<>();
             articulos.add(new android.example.com.miniproyecto1_equipo4.Articulo("Harina PAN", "1 KG","Cantidad: x2","20,00Bs", R.drawable.harina));
-            articulos.add(new android.example.com.miniproyecto1_equipo4.Articulo("Agua Minalba", "1 L", "Cantidad: x3","40,00Bs",R.drawable.agua));
+            articulos.add(new android.example.com.miniproyecto1_equipo4.Articulo("Agua Minalba", "1 L", "Cantidad: x3","4,00Bs",R.drawable.agua));
             articulos.add(new Articulo("Captain Crunch", "794 Grs", "Cantidad: x1", "79,99Bs",R.drawable.capn));
             articulos.add(new Articulo("Nutella","13 Oz","Cantidad x1","99,99Bs",R.drawable.nutella));
+            articulos.add(new Articulo("Pizza","100gr","Cantidad x1","20Bs",R.drawable.pizza));
+            articulos.add(new Articulo("Nestea","450gr","Cantidad x3","60Bs",R.drawable.tea));
+            articulos.add(new Articulo("Toddy","400gr","Cantidad x1","33Bs",R.drawable.toddy));
+            articulos.add(new Articulo("Huevos","300gr","Cantidad x30","20Bs",R.drawable.huevos));
+            articulos.add(new Articulo("Coca-Cola","355ml","Cantidad x1","15Bs",R.drawable.cocacola));
+
+
         }
         private void inicializarAdaptador(){
             ComprasAdapter adapter = new ComprasAdapter(articulos);
