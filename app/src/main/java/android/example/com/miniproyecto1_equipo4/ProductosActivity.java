d046f160d0190666198e2b6d21483dcec6b9d0d6
alexdgn213 <alexdgn213@gmail.com>
2018-05-20 17:57:13 -0400

    Agregados los otros articulos faltantes

--- app/src/main/java/android/example/com/miniproyecto1_equipo4/ProductosActivity.java
+++ app/src/main/java/android/example/com/miniproyecto1_equipo4/ProductosActivity.java
@@ -44,15 +44,26 @@ public class ProductosActivity extends AppCompatActivity {
      * @param productsList : Lista de productos a llenar
      */
     private void inicializarData(LinkedList productsList){
-        productsList.add(new ArticuloComprar("Harina","Precio: 20,00Bs",R.drawable.harina));
-        productsList.add(new ArticuloComprar("Nutella","Precio: 99,99$",R.drawable.nutella));
-        productsList.add(new ArticuloComprar("Pizza","Precio: 20Bs",R.drawable.pizza));
-        productsList.add(new ArticuloComprar("Agua Minalba","Precio: 4,00Bs",R.drawable.agua));
-        productsList.add(new ArticuloComprar("Capitan Crunch","Precio: 79,99$",R.drawable.capn));
-        productsList.add(new ArticuloComprar("Nestea","Precio: 60,00Bs",R.drawable.tea));
-        productsList.add(new ArticuloComprar("Toddy","Precio: 33,00Bs",R.drawable.toddy));
-        productsList.add(new ArticuloComprar("Huevos","Precio: 20,00Bs",R.drawable.huevos));
-        productsList.add(new ArticuloComprar("Coca-Cola","Precio: 15,00Bs",R.drawable.cocacola));
+        productsList.add(new ArticuloComprar("Harina","Precio: 20,000Bs",R.drawable.harina));
+        productsList.add(new ArticuloComprar("Nutella","Precio: 2,000,000Bs",R.drawable.nutella));
+        productsList.add(new ArticuloComprar("Pizza","Precio: 2,000,000Bs",R.drawable.pizza));
+        productsList.add(new ArticuloComprar("Agua Minalba","Precio: 4,000Bs",R.drawable.agua));
+        productsList.add(new ArticuloComprar("Capitan Crunch","Precio: 50,000Bs",R.drawable.capn));
+        productsList.add(new ArticuloComprar("Nestea","Precio: 60,000Bs",R.drawable.tea));
+        productsList.add(new ArticuloComprar("Toddy","Precio: 33,000Bs",R.drawable.toddy));
+        productsList.add(new ArticuloComprar("Huevos","Precio: 20,000Bs",R.drawable.huevos));
+        productsList.add(new ArticuloComprar("Coca-Cola","Precio: 15,000Bs",R.drawable.cocacola));
+        productsList.add(new ArticuloComprar("Cebolla","Precio: 25,000Bs",R.drawable.cebolla));
+        productsList.add(new ArticuloComprar("Cilantro","Precio: 40,000Bs",R.drawable.cilantro));
+        productsList.add(new ArticuloComprar("Fresa","Precio: 20.000Bs",R.drawable.fresas));
+        productsList.add(new ArticuloComprar("Helado","Precio: 400,000Bs",R.drawable.helado));
+        productsList.add(new ArticuloComprar("Leche","Precio: 50,000Bs",R.drawable.leche));
+        productsList.add(new ArticuloComprar("Manzana","Precio: 65,000Bs",R.drawable.manzana));
+        productsList.add(new ArticuloComprar("Naranja","Precio: 32,000Bs",R.drawable.naranja));
+        productsList.add(new ArticuloComprar("Oreo","Precio: 800,000Bs",R.drawable.oreo));
+        productsList.add(new ArticuloComprar("Patilla","Precio: 150,000Bs",R.drawable.patilla));
+        productsList.add(new ArticuloComprar("Papa","Precio: 200,000Bs",R.drawable.papa));
+        productsList.add(new ArticuloComprar("Mantequilla","Precio: 15,000Bs",R.drawable.mantequilla));
     }
 
     /**
